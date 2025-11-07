932b487e77c0fd535376803b1b2059aac552fab6
Nuwa-IT <rosnuwa@gmail.com>
2025-11-08 01:03:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -76,4 +76,15 @@ function submitOrder() {
         return false;
     }
 }
+function openImage(src) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+    lightboxImg.src = src;
+    lightbox.style.display = "flex";
+}
+
+function closeImage() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.style.display = "none";
+}
 
